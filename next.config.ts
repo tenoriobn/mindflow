6fec1b1952da4cdf5d8fdfbed33743f325ccd5f6
Bruno Tenorio <tenoriobn@gmail.com>
2026-05-19 16:15:00 -0300

    refactor: :zap: Ajusta imagens para melhor desempenho e carregamento

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/webp'],
+
+    qualities: [75, 85, 90, 95, 100],
+
+    deviceSizes: [640, 750, 828, 1080, 1200, 1440, 1920, 2048, 2560, 3840],
+  },
   turbopack: {
     rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
   },
--- src/app/(home)/HeroSection/index.tsx
+++ src/app/(home)/HeroSection/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import './hero-section.css';
 import PartnerLogoCarousel from './PartnerLogoCarousel';
 
@@ -8,8 +9,30 @@ const HeroSection = () => {
       aria-labelledby="hero-section-title"
       className="3xl:pt-[7.913vw] flex min-h-svh w-full flex-col pt-23 md:pt-31 xl:pt-35"
     >
-      <div className="hero__background before:bg-gradient-hero-section-bg-overlay 3xl:p-[1.667vw] md:rounded-r-[2.5rem 3xl:rounded-tl-[2.083vw] 3xl:rounded-r-[2.083vw] relative isolate grid flex-1 items-center overflow-hidden rounded-tl-[1.25rem] rounded-r-[1.25rem] bg-slate-950 bg-[url(/images/bg-herosection.png)] p-4 md:rounded-tl-[2.5rem] md:rounded-r-[2.5rem] md:p-8">
-        <header className="grid" style={{ gridTemplateColumns: 'min-content' }}>
+      <div className="3xl:p-[1.667vw] 3xl:rounded-tl-[2.083vw] 3xl:rounded-r-[2.083vw] relative isolate grid flex-1 items-center overflow-hidden rounded-tl-[1.25rem] rounded-r-[1.25rem] bg-slate-950 p-4 md:rounded-tl-[2.5rem] md:rounded-r-[2.5rem] md:p-8">
+        <Image
+          src="/images/bg-herosection.webp"
+          alt="Imagem de fundo com mulher respirando profundamente em um ambiente floral"
+          aria-hidden="true"
+          fill
+          preload
+          fetchPriority="high"
+          loading="eager"
+          quality={90}
+          sizes="
+            (max-width: 768px) 100vw,
+            (max-width: 1440px) 1400px,
+            1920px
+          "
+          className="object-cover object-[calc(81%+120px)_center] md:object-[calc(84%+120px)_center] xl:object-center"
+        />
+
+        <div
+          aria-hidden
+          className="bg-gradient-hero-section-bg-overlay absolute h-full w-full bg-size-[100%_100%,165%_200%] md:bg-size-[100%_100%,160%_200%] lg:bg-size-[100%_100%,140%_200%] xl:bg-cover"
+        />
+
+        <header className="relative z-10 grid" style={{ gridTemplateColumns: 'min-content' }}>
           <h2
             id="hero-section-title"
             className="3xl:text-[4.8vw] 3xl:mb-[1.667vw] mb-4 text-[8.8vw] font-semibold whitespace-nowrap text-white/95 md:mb-8 md:text-[4.125rem]"
