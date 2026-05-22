09c0fdb38e11f88fc706e2d97b03e74253b2f6b7
Bruno Tenorio <tenoriobn@gmail.com>
2026-05-22 12:10:30 -0300

    feat: :sparkles: Instala GSAP e configura registro centralizado no projeto

---
+++ src/lib/gsap.ts
@@ -0,0 +1,8 @@
+'use client';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+export { gsap, useGSAP, ScrollTrigger };
